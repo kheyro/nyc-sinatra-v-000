800b02e57cd2006fb814fb2591d53f3f51c69de2
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-13 12:56:16 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,14 @@
+class FiguresController < ApplicationController
+
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'/figures/new'
+  end
+  
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+end
